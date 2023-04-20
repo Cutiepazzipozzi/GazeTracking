ab9b702bc68de5153f8c35f7aafec3bd73e5dbbf
cutiepazzipozzi <sk000801@naver.com>
2023-04-20 22:48:45 +0900

    feat: Python flask를 활용 -> 로컬 서버 실시간 분석

--- example.py
+++ example.py
@@ -2,43 +2,62 @@
 Demonstration of the GazeTracking library.
 Check the README.md for complete documentation.
 """
-
+from flask import Flask, Response, render_template
 import cv2
+import platform
 from gaze_tracking import GazeTracking
 
 gaze = GazeTracking()
 webcam = cv2.VideoCapture(0)
+app = Flask(__name__)
 
-while True:
-    # We get a new frame from the webcam
-    _, frame = webcam.read()
+def gen_frames():  
+    while True:
+        # We get a new frame from the webcam
+        _, frame = webcam.read()
 
-    # We send this frame to GazeTracking to analyze it
-    gaze.refresh(frame)
+        # We send this frame to GazeTracking to analyze it
+        gaze.refresh(frame)
 
-    frame = gaze.annotated_frame()
-    text = ""
+        frame = gaze.annotated_frame()
+        text = ""
 
-    if gaze.is_blinking():
-        text = "Blinking"
-    elif gaze.is_right():
-        text = "Looking right"
-    elif gaze.is_left():
-        text = "Looking left"
-    elif gaze.is_center():
-        text = "Looking center"
+        if gaze.is_blinking():
+            text = "Blinking"
+        elif gaze.is_right():
+            text = "Looking right"
+        elif gaze.is_left():
+            text = "Looking left"
+        elif gaze.is_center():
+            text = "Looking center"
 
-    cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
+        cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
-    left_pupil = gaze.pupil_left_coords()
-    right_pupil = gaze.pupil_right_coords()
-    cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-    cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        left_pupil = gaze.pupil_left_coords()
+        right_pupil = gaze.pupil_right_coords()
+        cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
-    cv2.imshow("Demo", frame)
+        # cv2.imshow("Demo", frame)
 
-    if cv2.waitKey(1) == 27:
-        break
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+        if cv2.waitKey(1) == 27:
+            break
    
-webcam.release()
-cv2.destroyAllWindows()
+# webcam.release()
+# cv2.destroyAllWindows()
+
+@app.route('/')
+def index():
+    return render_template('liveCam.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run('127.0.0.1', 8080, debug=True)
